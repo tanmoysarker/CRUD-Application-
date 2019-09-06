ab846762fbef32e940d94ff8528bda4e2940b2c2
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-09-06 13:42:40 +0600

    Updates

---
+++ precache-manifest.2568413e68ca10f94304855cb1e745d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c9010eebc216b6e247291079989072",
+    "url": "/CRUD-Application-/index.html"
+  },
+  {
+    "revision": "e8aef2645c33cf94d58f",
+    "url": "/CRUD-Application-/static/css/main.3b32bf20.chunk.css"
+  },
+  {
+    "revision": "c161ea5cca505f4e097e",
+    "url": "/CRUD-Application-/static/js/2.467a7ded.chunk.js"
+  },
+  {
+    "revision": "e8aef2645c33cf94d58f",
+    "url": "/CRUD-Application-/static/js/main.ad849b07.chunk.js"
+  },
+  {
+    "revision": "4a6862cd0262538277c7",
+    "url": "/CRUD-Application-/static/js/runtime~main.b5995982.js"
+  }
+]);
